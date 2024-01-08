c44e2f223add5d7088ecfe501f8d9bb7bc294ae2
CharlesDESC <charles.deschamps@ynov.com>
2024-01-08 22:55:12 +0100

    ajout dialog box pour le panier + fonctionement

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { CartService } from '../services/add-cart.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCartComponent } from '../dialog-cart/dialog-cart.component';
 
 @Component({
   selector: 'app-cart',
@@ -12,12 +14,23 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CartComponent {
   cartItems: any[] = [];
-  isShowCart: boolean = false;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, public dialog: MatDialog) {}
 
-  onDisplayCart() {
-    this.isShowCart = !this.isShowCart;
+  openDialog(): void {
     this.cartItems = this.cartService.getCartItems();
+
+    const dialogRef = this.dialog.open(DialogCartComponent, {
+      width: '450px',
+      height: '500px',
+      data: {
+        cartItems: this.cartItems,
+      },
+    });
+
+    // Écoute la fermeture de la boîte de dialogue
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('La boîte de dialogue est fermée', result);
+    });
   }
 }
---
+++ src/app/dialog-cart/dialog-cart.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-dialog-cart',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './dialog-cart.component.html',
+  styleUrls: ['./dialog-cart.component.css'],
+})
+export class DialogCartComponent {
+  cartItems: any[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<DialogCartComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {
+    this.cartItems = this.data.cartItems;
+  }
+
+  calculateTotal(): number {
+    return this.cartItems.reduce((total, cartItem) => {
+      return total + cartItem.product.price * cartItem.quantity;
+    }, 0);
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
