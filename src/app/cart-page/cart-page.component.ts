52946ecf88d023215a54a6dc9ed2b9e7798b410e
Théo_CATALA <93990201+tctla@users.noreply.github.com>
2024-01-16 15:06:04 +0100

    Card OK

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatCardModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css',
 })
